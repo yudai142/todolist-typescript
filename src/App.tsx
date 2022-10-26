3e3b2680830dbeca62f77c29cca1f7ccb0b9501b
yudai142 <yudai.shimoji.42865233@gmail.com>
2022-10-26 13:06:03 +0900

    ボタンを押してもリロードしない処理作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+  
+  type Todo = {
+    inputValue: string;
+    id: number;
+    checked: boolean;
+  };
+  
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // console.log(e.target.value);
+    setInputValue(e.target.value);
+  };
+  
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  }
   return (
     <div className="App">
       <div>
         <h2>ToDoリスト with Typescript</h2>
-        <form onSubmit={()=>{}}>
-          <input type="text" onChange={()=>{}} className="inputText" />
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <input type="text" onChange={(e)=> handleChange(e)} className="inputText" />
           <input type="submit" value="作成" className='submitButton' />
         </form>
       </div>
