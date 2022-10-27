eee2d9d4f0f5020a81c6fa734f403ca743d116a3
yudai142 <yudai.shimoji.42865233@gmail.com>
2022-10-27 13:48:40 +0900

    編集機能作成

--- src/App.tsx
+++ src/App.tsx
@@ -40,6 +40,17 @@ function App() {
     setTodos(newTodos);
   }
   
+  const handleChecked = (id: number, checked: boolean) => {
+    const newTodos = todos.map((todo) => {
+      if(todo.id === id) {
+        todo.checked = !checked;
+      }
+      return todo;
+    });
+    
+    setTodos(newTodos);
+  }
+  
   return (
     <div className="App">
       <div>
@@ -51,7 +62,8 @@ function App() {
         <ul className="todoList">
           {todos.map((todo) => (
             <li key={todo.id}>
-              <input type="text" onChange={(e)=> handleEdit(todo.id, e.target.value)} className="inputText" value={todo.inputValue} />
+              <input type="text" onChange={(e)=> handleEdit(todo.id, e.target.value)} className="inputText" value={todo.inputValue} disabled={todo.checked} />
+              <input type="checkbox" onChange={(e)=> handleChecked(todo.id, todo.checked)} className="inputText"/>
             </li>
           ))}
         </ul>
